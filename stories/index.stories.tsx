d3213dc84651b1cf84ed13969092d6cf72c53f36
Inaki Anduaga <inaki.anduaga@scout24.com>
2018-07-25 01:41:57 +0200

    Add knob example to the button story

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -2,15 +2,25 @@ import React from 'react'
 
 import { storiesOf, addDecorator } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs'
 import Button from '../src/components/button/Button'
 
 // addDecorator(withInfo({ inline: true }))
 
-storiesOf('Button', module).add('with different styles', () => (
-  <div>
-    <Button type="ross">Ross</Button> &nbsp;
-    <Button type="bob">Bob</Button> &nbsp;
-    <Button type="ghost">Ghost</Button> &nbsp;
-    <Button type="marketing">Marketing</Button>
-  </div>
-))
+storiesOf('Button', module)
+  .addDecorator(withKnobs)
+  .add('with different styles', () => (
+    <div>
+      <Button type="ross">Ross</Button> &nbsp;
+      <Button type="bob">Bob</Button> &nbsp;
+      <Button type="ghost">Ghost</Button> &nbsp;
+      <Button type="marketing">Marketing</Button>
+    </div>
+  ))
+  .add('parametrized', () => (
+    <div>
+      <Button type="ross" disabled={boolean('Disabled', false)}>
+        {text('Label', 'Ross')}
+      </Button>
+    </div>
+  ))
