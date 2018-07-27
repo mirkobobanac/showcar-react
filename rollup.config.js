3da4378949570070b1b2165f3a4d043ab8495c78
Inaki Anduaga <inaki.anduaga@scout24.com>
2018-07-27 22:22:21 +0200

    Add library bundle via rollup config
    We generate a CJS bundle for regular library consumption into other projects and also a iifs browser-friendly export that dumps all modules into window.ShowcarReact so it can be referenced as an external in other projects

---
+++ rollup.config.js
@@ -0,0 +1,79 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+import typescript from 'rollup-plugin-typescript2'
+import json from 'rollup-plugin-json'
+import { terser } from 'rollup-plugin-terser'
+
+const pkg = require('./package.json')
+
+export default [
+  /**
+   * Rollup configuration for bundling the library
+   * https://github.com/alexjoverm/typescript-library-starter
+   */
+  {
+    input: `src/index.ts`,
+    output: [
+      { file: pkg.main, format: 'cjs', sourcemap: true }, //
+      { file: pkg.module, format: 'es', sourcemap: true }
+    ],
+    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+    external: ['react'],
+    watch: {
+      include: 'src/**'
+    },
+    plugins: [
+      // Allow json resolution
+      json(),
+      // Compile TypeScript files
+      typescript({ useTsconfigDeclarationDir: true, tsconfig: 'tsconfig.prod.json' }),
+      // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+      commonjs(),
+      // Allow node_modules resolution, so you can use 'external' to control
+      // which external modules to include in the bundle
+      // https://github.com/rollup/rollup-plugin-node-resolve#usage
+      resolve(),
+      // Resolve source maps to the original source
+      sourceMaps()
+    ]
+  },
+
+  /**
+   * Rollup configuration for bundling library as application that exposes library in external window global
+   * https://mixmax.com/blog/rollup-externals
+   */
+  {
+    input: `src/index.ts`,
+    output: {
+      file: pkg.browser,
+      format: 'iife',
+      sourcemap: false,
+      name: 'window.ShowcarReact', // name under which the output will be available globally (window)
+      globals: {
+        react: 'React'
+      }
+    },
+    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+    external: ['react'],
+    watch: {
+      include: 'src/**'
+    },
+    plugins: [
+      // Allow json resolution
+      json(),
+      // Compile TypeScript files
+      typescript({ useTsconfigDeclarationDir: true, tsconfig: 'tsconfig.prod.json' }),
+      // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+      commonjs(),
+      // Allow node_modules resolution, so you can use 'external' to control
+      // which external modules to include in the bundle
+      // https://github.com/rollup/rollup-plugin-node-resolve#usage
+      resolve(),
+      // Resolve source maps to the original source
+      sourceMaps(),
+      // Minify code
+      terser()
+    ]
+  }
+]
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Export all components from single file
+// https://stackoverflow.com/questions/34072598/es6-exporting-importing-in-index-file
+// https://github.com/ReactiveX/rxjs/blob/master/src/index.ts
+
+export { default as Button } from './components/button/Button'
+export { default as OutsideClickDetector } from './components/outsideclickdetector/OutsideClickDetector'
