8ec1b03a3800c92e11adb05a6ab9c1daa7028ba3
Inaki Anduaga <inaki.anduaga@scout24.com>
2018-07-29 14:04:54 +0200

    Add storyshots integration
    - Babel used to process plain JS files in addition to TS jest to process typescript
    - Additional plugin needed to handle webpack's `require.context`

---
+++ __tests__/stories/stories.test.ts
@@ -0,0 +1,6 @@
+import registerRequireContextHook from 'babel-plugin-require-context-hook/register'
+registerRequireContextHook()
+
+import initStoryshots from '@storybook/addon-storyshots'
+
+initStoryshots()
